0df600bb843591e15bb0b8b6136da1767e6d9d2a
Yinghao Lei <leiyinghao.bj@gmail.com>
2014-03-26 13:21:41 +0800

    init  proj

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Orcl::Application.config.secret_key_base = '322585b512feb4943c25c4e19ea736e5b176c4b5dca89412839a544d5a912095faef0d0ff59f2a7a6fe3bb792367f1c3e5c789bb8e83499980a0aeb1388306bb'
